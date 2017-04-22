70fff8eb9eadd08ca42425c688f226a103f33613
David Koval <kovaldavid@hotmail.com>
2017-04-22 04:17:56 -0500

    Added Tests to tester and wrote test plan

--- app/src/test/java/max/sudoku/UnitTest.java
+++ app/src/test/java/max/sudoku/UnitTest.java
@@ -6,7 +6,6 @@ import static org.junit.Assert.*;
 
 public class UnitTest {
     MainActivity m = new MainActivity();
-
     @Test
     public void solveHorizontalCorrect() throws Exception {
         m.gridVal=new int[9][9];
@@ -143,7 +142,7 @@ public class UnitTest {
     @Test
     public void whiteBoxTestCorrect() throws Exception {
         m.gridVal=new int[9][9];
-        String s = "102004070000902800009003004000240006000107000400068000200800700007501000080400109";
+        String s = "000427000060090080000000000900000008120030045500000007000000000040060030000715004";
         for(int i=0;i<9;i++)for(int j=0;j<9;j++){
             m.gridVal[j][i]=Integer.parseInt(s.charAt(i*9+j)+"");
         }
@@ -158,5 +157,18 @@ public class UnitTest {
             m.gridVal[j][i]=Integer.parseInt(s.charAt(i*9+j)+"");
         }
         assertEquals(false,m.solveVertical() && m.solveHorizontal() && m.solveBox() && m.solveRecur(0,0));
+    }
+
+    @Test
+    public void performanceTest() throws Exception{
+        m.gridVal=new int[9][9];
+       String s =    "800000000003600000070090200050007000000045700000100030001000069008500010090000400";
+        for(int i=0;i<9;i++)for(int j=0;j<9;j++){
+            m.gridVal[j][i]=Integer.parseInt(s.charAt(i*9+j)+"");
+        }
+        long startTime = System.currentTimeMillis();
+        boolean t = m.solveRecur(0,0);
+        long endTime = System.currentTimeMillis();
+        assertEquals(true, t&&(endTime-startTime)/1000<5);
     }
 }
